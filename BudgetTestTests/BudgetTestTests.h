d164a3ca01df1f1d74a50377109ce0e0c362ee2e
Joe Cortopassi <joe@joecortopassi.com>
2011-09-04 21:09:35 -0700

    Initial Commit

---
+++ BudgetTestTests/BudgetTestTests.h
@@ -0,0 +1,13 @@
+//
+//  BudgetTestTests.h
+//  BudgetTestTests
+//
+//  Created by Joe Cortopassi on 9/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BudgetTestTests : SenTestCase
+
+@end
---
+++ BudgetTestTests/BudgetTestTests.m
@@ -0,0 +1,32 @@
+//
+//  BudgetTestTests.m
+//  BudgetTestTests
+//
+//  Created by Joe Cortopassi on 9/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "BudgetTestTests.h"
+
+@implementation BudgetTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BudgetTestTests");
+}
+
+@end
